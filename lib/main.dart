0a9eb80c0a5fb8bb7882e4561c00cfc03b334143
Andreas F <anden_91_@hotmail.com>
2022-09-06 12:06:07 +0200

    Kommentarer borttagna. Påbörjat ändringar av layout / test.

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text("TIG333 TODO-list"),
+            ),
+          ),
+          body: Center(
+            child: Text("Här kommer en lista"),
+          )),
     );
   }
 }
