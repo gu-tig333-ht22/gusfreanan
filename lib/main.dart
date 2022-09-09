fc888d3e3eb67feb7e9a2e5d65b2f20c1692ce0a
Andreas F <anden_91_@hotmail.com>
2022-09-09 15:53:09 +0200

    Steg 1: Ändrat färgsättning, text samt småtestat en funktion för att lägga till rader med symboler på skärmen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//Debug painting: Choose "Toggle Debug Paint" from command palette
 
 void main() {
   runApp(const MyApp());
@@ -7,70 +6,56 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO-list',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child: Text("TIG333 TODO-list"),
-            ),
-          ),
-          body: Center(
-            child: Text("Här kommer en lista"),
-          )),
+      home: const MyHomePage(title: 'TIG333 TODO-list'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  //FUNKTION SOM ANVÄNDS FÖR ATT ÖKA ANTALET I "TODO-list" PÅ SKÄRMEN
+  int tal = 0;
+  var _lista = <int>[];
+  void _increaseList() {
     setState(() {
-      _counter++;
+      _lista.add(tal);
+      tal++;
     });
   }
+  //SLUT FUNKTION
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: _lista.length,
+            itemBuilder: (BuildContext context, int tal) {
+              return ListTile(
+                  leading: const Icon(Icons.check_box_outlined),
+                  trailing: const Icon(Icons.close),
+                  title: Text("Valfri uppgift nr. $tal"));
+            }),
+        floatingActionButton: FloatingActionButton(
+            onPressed:
+                _increaseList, //KALLAR FUNKTION SOM ÖKAR ANTALET I "TODO-list"
+            tooltip: 'Add to list',
+            child: const Icon(Icons.add)));
   }
 }
